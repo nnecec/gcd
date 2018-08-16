383de52b43c68e84eba4061eb3321a8a441ed39d
nnecec <nnecec@outlook.com>
2018-08-16 12:43:03 +0800

    add debug in vscode

--- src/FormMap/index.tsx
+++ src/FormMap/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Form, Input, Select } from 'antd'
 
-interface FormItem {
+const Option = Select.Option
+
+// 表单配置 antd 组件 配置
+interface ItemTypeConfig {
+  name: string, // 组件类型
+  configs: object, // 组件 options
+  options: any[], // select option 选项
+}
+// 表单子项 配置
+interface FormItemConfig {
   key: string,
   initialValue: any,
-  type: 'input' | 'select'
+  type: ItemTypeConfig
 }
 interface FormMapProps {
-  items: FormItem[]
+  items: FormItemConfig[]
 }
 export default class FormMap extends React.Component<FormMapProps, any> {
-  private static defaultProps = {
-    length: Infinity
-  }
   private state: {
     previewVisible: boolean // 预览图片 Modal 的 Visible 属性
     previewImage: string // 预览图片的 url
@@ -28,37 +34,27 @@ export default class FormMap extends React.Component<FormMapProps, any> {
     )
   }
 
-  private getItemByType = (item) => {
-    const { type, configs } = item
-    let FieldItem
-
-    switch (type) {
-      case 'Input':
-        FieldItem = <Input  {...configs} />
-        break
-      case 'Select':
-        FieldItem = <Select  {...configs} />
-        break
-      default:
-        FieldItem = <Input  {...configs} />
-    }
-
-    return FieldItem
-  }
   private renderItem = item => {
     const { form } = this.props
     const { getFieldDecorator } = form
-    const { key, initialValue, FormItemConfigs } = item
+    const { key, initialValue, formItemConfigs } = item
+
+    const ITEM_CONFIG = {
+      label: key,
+      labelCol: { span: 6 },
+      wrapperCol: { span: 14 },
+      ...formItemConfigs
+    }
 
     return (
       <Form.Item
-        {...FormItemConfigs}
+        {...ITEM_CONFIG}
       >
         {
           getFieldDecorator(key, {
             initialValue: initialValue || undefined,
           })(
-            this.getItemByType(item)
+            getItemByType(item)
           )}
       </Form.Item>
     )
@@ -76,6 +72,30 @@ export default class FormMap extends React.Component<FormMapProps, any> {
   }
 }
 
+// 针对不同的
+function getItemByType(type: ItemTypeConfig) {
+  let FieldItem
 
+  const CONFIGS = {
+    ...type.configs
+  }
 
+  switch (type.name) {
+    case 'Input':
+      FieldItem = <Input  {...CONFIGS} />
+      break
+    case 'Select':
+      FieldItem = <Select style={{ width: '200px' }} {...CONFIGS} >
+        {
+          type.options.map(option => (
+            <Option key={option.value} value={option.value}>{option.mean}</Option>
+          ))
+        }
+      </Select >
+      break
+    default:
+      FieldItem = <Input  {...CONFIGS} />
+  }
 
+  return FieldItem
+}
