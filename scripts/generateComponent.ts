3090147bed7ae53f587bcf764115e4296690d02c
nnecec <nnecec@outlook.com>
2018-10-25 11:19:45 +0800

    update

--- scripts/generateComponent.ts
+++ scripts/generateComponent.ts
@@ -32,21 +32,22 @@ inquirer
         return
       }
 
-      // 写入 index.tsx
-      const INDEX_TSX = `import * as React from 'react'\nimport { Button } from 'antd\n'
-    `
+      // index.tsx
+      const INDEX_PROPS = `${name}Props`
+      const INDEX_TSX = `import * as React from 'react'\nimport { Button } from 'antd'\n\nimport { ${INDEX_PROPS} } from './i${name}'\n\nexport default class ${name} extends React.Component<${INDEX_PROPS}, any> {\n\tpublic props: ${INDEX_PROPS}\n\tpublic state: {\n\n\t}\n\tconstructor(props) {\n\t\tsuper(props)\n\t}\n\n\tpublic render() {\n\t\treturn (<div></div>)\n\t}\n}\n`
       fs.writeFile(`${route}/${name}/index.tsx`, INDEX_TSX, (err) => { })
 
+      // interface
+      const INTERFACE_TS = `export interface ${INDEX_PROPS} {\n\n}\n`
+      fs.writeFile(`${route}/${name}/i${name}.ts`, INTERFACE_TS, (err) => { })
 
-      // 写入 index.mdx
+      // index.mdx
       const INDEX_MDX = `---\nname: ${name}\nroute: /components/type/${name.toLowerCase()}\nmenu: ${firstUpperCase(type)} Components\n---\n\nimport { Playground, PropsTable } from 'docz'\nimport { ${name} } from 'hawkeye-arrow'\n`
-
       fs.writeFile(`${route}/${name}/index.mdx`, INDEX_MDX, (err) => { })
 
       // 添加到 type/index.ts 中
       let INDEX_TS = fs.readFileSync(`${route}/index.ts`, "utf-8")
       INDEX_TS += `export { default as ${name} } from './${name}'\n`
-
       fs.writeFile(`${route}/index.ts`, INDEX_TS, (err) => { })
     })
 
